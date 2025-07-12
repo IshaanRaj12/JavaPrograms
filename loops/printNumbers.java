5e04bea840bdd2de1b7ae2abf5c7192836acd0d4
IshaanRaj12 <raj.ishaan.12@gmail.com>
2025-07-12 15:21:02 -0500

    Create printNumbers.java

---
+++ loops/printNumbers.java
@@ -0,0 +1,9 @@
+/* Print numbers from 1-10 */
+
+public class Main {
+  public static void main(String args[]){
+    for (int i = 1; i < 11; i++) {
+      System.out.println("Num: " + i);
+    } 
+  }
+}
